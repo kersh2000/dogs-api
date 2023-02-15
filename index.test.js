8306f44157b410d2ffc8e14aba0d61ac6a44987c
kersh2000 <bradleykershaw333@gmail.com>
2023-02-15 09:36:00 +0000

    Added GET and POST TDDs

--- index.test.js
+++ index.test.js
@@ -7,6 +7,18 @@ const { sequelize, Dog } = require('./db');
 const seed = require('./db/seedFn');
 const {dogs} = require('./db/seedData');
 
+function clean_objects(res_obj, db_obj){
+    const responseData = Object.assign({}, res_obj, {
+        createdAt: undefined,
+        updatedAt: undefined,
+      });
+    const dbData = Object.assign({}, db_obj.toJSON(), {
+    createdAt: undefined,
+    updatedAt: undefined,
+    });
+    return [responseData, dbData]
+}
+
 describe('Endpoints', () => {
     // to be used in POST test
     const testDogData = {
@@ -33,4 +45,27 @@ describe('Endpoints', () => {
             expect(response.body[0]).toEqual(expect.objectContaining(dogs[0]));
         });
     });
+
+    describe('POST /dogs', () => {
+        let response;
+        it('should successfully create a new dog entry with correct data into the DB', async () => {
+            // make a request
+            response = await request(app).post('/dogs').send(testDogData)
+            // expect matching data
+            expect(response.body).toEqual(expect.objectContaining(testDogData))
+        })
+
+        it('should match the data in the DB', async () => {
+            // get the newly created dog from the database using the id from the response
+            const db_dog = await Dog.findByPk(response.body.id);
+          
+            // create a new object that contains only the properties to compare
+            const values = clean_objects(response.body, db_dog)
+          
+            // compare the two objects
+            expect(values[0]).toEqual(values[1]);
+          });
+          
+          
+    })
 });
